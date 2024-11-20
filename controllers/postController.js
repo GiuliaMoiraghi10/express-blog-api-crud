844ae3ae569af3c6e00384348ead212be40ff6f9
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2024-11-20 17:09:42 +0100

    Aggiunta milestone 2 - index show e destroy

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,12 +1,26 @@
 const posts = require('../data/posts.js')
 
 function index(req, res) {
-    res.send('Lista dei post')
+    // res.send('Lista dei post')
+    res.json(posts)
 }
 
 function show(req, res) {
     const id = parseInt(req.params.id)
-    res.send(`Dettagli del post con id ${id}`)
+    // res.send(`Dettagli del post con id ${id}`)
+    let post = posts.find(post => post.id === id)
+
+    if (!post) {
+
+        res.status(404)
+
+        return res.json({
+            error: 'Not found',
+            message: 'Post non trovato'
+        })
+    }
+
+    res.json(post)
 }
 
 function store(req, res) {
@@ -25,7 +39,25 @@ function modify(req, res) {
 
 function destroy(req, res) {
     const id = parseInt(req.params.id)
-    res.send(`Elimina post con id ${id}`)
+    // res.send(`Elimina post con id ${id}`)
+    const post = posts.findIndex(post => post.id === id)
+
+    if (!post) {
+        res.status(404)
+
+        return res.json({
+            status: 404,
+            error: 'Not Found',
+            message: 'Post non trovato'
+        })
+    }
+
+    posts.splice(posts.indexOf(post), 1)
+
+    console.log(posts)
+
+    res.sendStatus(204)
+
 }
 
 module.exports = { index, show, store, update, modify, destroy }
